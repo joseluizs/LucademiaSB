ba34180bf0788d050134b2a6620f80608de6bfcb
joseluizs <joseluiz.smarques@gmail.com>
2023-02-22 20:29:32 -0300

    HTML aluno

---
+++ Loucademia/src/main/java/br/com/luiz/loucademia/Config.java
@@ -0,0 +1,10 @@
+package br.com.luiz.loucademia;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.faces.annotation.FacesConfig;
+
+@FacesConfig
+@ApplicationScoped
+public class Config {
+	
+}
