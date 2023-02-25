d2c2bca19917e1eacd1f0362621a67032d2c3541
joseluizs <joseluiz.smarques@gmail.com>
2023-02-25 09:15:27 -0300

    Classe Aluno

---
+++ Loucademia/src/main/java/br/com/luiz/loucademia/domain/aluno/Aluno.java
@@ -0,0 +1,73 @@
+package br.com.luiz.loucademia.domain.aluno;
+
+import java.time.LocalDate;
+
+public class Aluno {
+	
+	public enum Sexo {
+		MASCULINO, FEMININO;
+	}
+	
+	public enum Situacao{
+		ATIVO, INATIVO, PENDENTE;
+	}
+	
+	private String matricula;	
+	private String nome;
+	private Sexo sexo;
+	private Integer rg;
+	private LocalDate dataNascimento;
+	private Situacao situacao;
+	private Endereco endereco = new Endereco();
+	private Telefone telefone = new Telefone();
+	public String getMatricula() {
+		return matricula;
+	}
+	public void setMatricula(String matricula) {
+		this.matricula = matricula;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public Sexo getSexo() {
+		return sexo;
+	}
+	public void setSexo(Sexo sexo) {
+		this.sexo = sexo;
+	}
+	public Integer getRg() {
+		return rg;
+	}
+	public void setRg(Integer rg) {
+		this.rg = rg;
+	}
+	public LocalDate getDataNascimento() {
+		return dataNascimento;
+	}
+	public void setDataNascimento(LocalDate dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+	public Situacao getSituacao() {
+		return situacao;
+	}
+	public void setSituacao(Situacao situacao) {
+		this.situacao = situacao;
+	}
+	public Endereco getEndereco() {
+		return endereco;
+	}
+	public void setEndereco(Endereco endereco) {
+		this.endereco = endereco;
+	}
+	public Telefone getTelefone() {
+		return telefone;
+	}
+	public void setTelefone(Telefone telefone) {
+		this.telefone = telefone;
+	}
+	
+
+}
