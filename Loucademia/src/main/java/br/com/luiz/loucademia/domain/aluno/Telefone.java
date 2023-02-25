2a3ef31b0fc60ab95853c9ad89e9b18a1e5ee5e0
joseluizs <joseluiz.smarques@gmail.com>
2023-02-25 09:09:35 -0300

    Classe Telefone

---
+++ Loucademia/src/main/java/br/com/luiz/loucademia/domain/aluno/Telefone.java
@@ -0,0 +1,36 @@
+package br.com.luiz.loucademia.domain.aluno;
+
+public class Telefone {
+	
+	private Integer dddCelular;
+	private Integer numeroCelular;
+	private Integer dddFixo;
+	private Integer numeroFixo;
+	public Integer getDddCelular() {
+		return dddCelular;
+	}
+	public void setDddCelular(Integer dddCelular) {
+		this.dddCelular = dddCelular;
+	}
+	public Integer getNumeroCelular() {
+		return numeroCelular;
+	}
+	public void setNumeroCelular(Integer numeroCelular) {
+		this.numeroCelular = numeroCelular;
+	}
+	public Integer getDddFixo() {
+		return dddFixo;
+	}
+	public void setDddFixo(Integer dddFixo) {
+		this.dddFixo = dddFixo;
+	}
+	public Integer getNumeroFixo() {
+		return numeroFixo;
+	}
+	public void setNumeroFixo(Integer numeroFixo) {
+		this.numeroFixo = numeroFixo;
+	}
+	
+	
+
+}
